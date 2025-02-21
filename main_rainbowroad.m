bf087cf2a3e07574fd0320b36546758e2352385d
julakshah <julakshah@gmail.com>
2025-02-21 14:36:46 -0500

    defined constants

--- main_rainbowroad.m
+++ main_rainbowroad.m
@@ -8,3 +8,6 @@ x = 0.3960 * cos(2.65 * (u + 1.4));
 y = 0.99 * sin(u + 1.4);
 % Define the range of parametric curves
 u_scope = [0, 3.2];
+
+MAX_WHEEL_SPEED = .3; % m/s
+WHEEL_BASE = .245; % meters
