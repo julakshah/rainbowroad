fbb394c8c75a03f9b39fe1b2a6d367ba8950ae5c
ncbanse <ncbanse@icloud.com>
2025-02-26 22:11:03 -0500

    Update main_rainbowroad.m
    added the comparison plot, and failed at implementing the normal and tangent vectors on the discrete experimental data.

--- main_rainbowroad.m
+++ main_rainbowroad.m
@@ -193,7 +193,7 @@ neato_data = neatov3.receive();
 % start the timer
 tic;
 % loop until 60 seconds have passed
-while toc<=60
+while toc<=30
     % set t_in to the amount of time that has elapsed
     t_in = toc;
      
@@ -212,22 +212,79 @@ while toc<=60
     neato_data = [neato_data; neatov3.receive()];
 end
 neatov3.setVelocities(0.0, 0.0);
-neato_data = neatov3.receive();
 neatov3.disconnect()
 %% NEATO COMPARE
-enc_data = neato_data.encoders();
-left_wheel_encoder_list = enc_data(2);
-right_wheel_encoder_list = enc_data(1);
+neato_data_size = size(neato_data)
+enc_data = zeros(neato_data_size(1), 2);
+for i = 1:length(neato_data)
+    enc_data(i, :) = neato_data(i).encoders();
+end
+left_wheel_encoder_list = enc_data(:, 2); % position data for left wheel
+right_wheel_encoder_list = enc_data(:, 1); % position data for right wheel
 
-dr = diff(right_wheel_encoder_list);
-dl = diff(left_wheel_encoder_list);
-theta = cumsum((dr - dl) ./ WHEEL_BASE);
-displacemennt = (dr + dl) ./ 2;
+dr = diff(right_wheel_encoder_list); % change in position for right wheel
+dl = diff(left_wheel_encoder_list); % change in position for left wheel
+theta = cumsum((dr - dl) ./ WHEEL_BASE); % theta vector
+theta = theta + .316;
+displacemennt = (dr + dl) ./ 2; % displacement vector
 
-x = cumsum(displacemennt .* -sin(theta));
-y = cumsum(displacemennt .* cos(theta));
+x_exp = cumsum(displacemennt .* cos(theta)); % x position vector
+y_exp = cumsum(displacemennt .* sin(theta)); % y position vector
 
-figure()
-plot(x, y)
-title('X & Y vs. Time')
-axis equal
+x_exp = x_exp - 0.33373270416764262492145011235346;
+y_exp = y_exp + 0.97559523268857557885287983301804;
+
+%{
+% SOLVE FOR TANGENTS
+% differentiate x and y functions
+dx_exp = diff(x_exp);
+dy_exp = diff(y_exp);
+% get x and y tangent equations
+tx_exp = dx_exp ./ displacemennt;
+ty_exp = dy_exp ./ displacemennt;
+
+% SOLVE FOR NORMALS
+% differentiate tx and ty
+dtx_exp = diff(tx_exp);
+dty_exp = diff(ty_exp);
+% find magnitude of T
+dt_mag_exp = sqrt(dtx_exp.^2 + dty_exp.^2);
+% get x and y normal equations
+nx_exp = dtx_exp / dt_mag_exp;
+ny_exp = dty_exp / dt_mag_exp;
+%}
+figure(1); clf;
+hold on
+axis square
+axis([-1.5,1.5,-1.5,1.5]);
+title("Comparing Measured and Experimental Path")
+xlabel("x position")
+ylabel("y position")
+% plot the center line
+plot(x_vals, y_vals, LineWidth=2);
+% add dots to ends of center line
+plot(x_vals(1),y_vals(1),'ko','markerfacecolor','k','markersize',5);
+plot(x_vals(end),y_vals(end),'ko','markerfacecolor','k','markersize',5);
+% plot tangent lines except the end points
+for i = 2:length(tu_vals)-1
+    plot(tnx(i),tny(i),'ko','markerfacecolor','k','markersize',5);
+    quiver(tnx(i), tny(i), tx_vals(i), ty_vals(i), ...
+        AutoScale="off",LineWidth=1.5, Color="#00841a")
+    quiver(tnx(i), tny(i), nx_vals(i), ny_vals(i), ...
+        AutoScale="off",LineWidth=1.5, Color="g")
+
+end
+plot(tnx(1),tny(1),'ko','markerfacecolor','r','markersize',10);
+
+plot(x, y, 'r--', LineWidth=2)
+
+% for i = 2:length(tx_exp)-1
+%     plot(x_exp(i),y_exp(i),'ko','markerfacecolor','k','markersize',5);
+%     quiver(x_exp(i), y_exp(i), tx_exp(i), ty_exp(i), ...
+%         AutoScale="off",LineWidth=1.5, Color="#00841a")
+%     quiver(x_exp(i), y_exp(i), nx_exp(i), ny_exp(i), ...
+%         AutoScale="off",LineWidth=1.5, Color="g")
+% end
+
+
+hold off
